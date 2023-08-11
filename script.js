af2e7675ba1cbbcc29455519cfd37df2e26e5e69
tristanwesendahlvelazquez <tristan.wesendahl@hotmail.com>
2023-08-11 11:31:00 +0200

    Deleting Bookmarks and Code Review

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarkContainer = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
+
 // Show Modal, focus on input
 function showModal() {
     modal.classList.add('show-modal');
@@ -33,6 +35,70 @@ function validate(nameValue, urlValue) {
     return true;
 }
 
+// Build our bookmarks Dom
+function buildBookmarks() {
+    // Build items
+    bookmarks.forEach((bookmark) => {
+        // Remove all bookmark elements
+        bookmarkContainer.textContent = '';
+       const {name, url} = bookmark;
+       // Item
+        const item = document.createElement('div');
+        item.classList.add('item');
+        // Close Icon
+        const closeIcon = document.createElement('i');
+        closeIcon.classList.add('fas','fa-times');
+        closeIcon.setAttribute('title','Delete Bookmarks');
+        closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+        // Favicon / Link Container
+        const linkInfo = document.createElement('div');
+        linkInfo.classList.add('name');
+        // Favicon
+        const favicon = document.createElement('img');
+        favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+        favicon.setAttribute('alt','Favicon');
+        // Link
+        const link = document.createElement('a');
+        link.setAttribute('href', `${url}`);
+        link.setAttribute('target','_blank');
+        link.textContent = name;
+        // Append to Bookmarks container
+        linkInfo.append(favicon, link);
+        item.append(closeIcon,linkInfo);
+        bookmarkContainer.appendChild(item);
+    });
+}
+
+// Fetch Bookmarks from local storage
+function fetchBookmarks() {
+    // get bookmarks from local storage if available
+    if (localStorage.getItem('bookmarks')){
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    }else {
+        // Create bookmarks array in localStorage
+        bookmarks = [
+            {
+                name: 'Jacinto Design',
+                url: 'https://jacinto.design',
+            },
+        ];
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+    buildBookmarks();
+}
+
+// Delete Bookmark
+function deleteBookmark(url) {
+    bookmarks.forEach((bookmark,i) => {
+       if (bookmark.url === url){
+           bookmarks.splice(i,1);
+       }
+    });
+    // Update Bookmarks array in localStorage, re-populate DOM
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    fetchBookmarks();
+}
+
 // Handle Data from Form
 function storeBookmark(e) {
     e.preventDefault();
@@ -45,7 +111,19 @@ function storeBookmark(e) {
     if (!validate(nameValue,urlValue)){
         return false;
     }
+    const bookmark = {
+        name: nameValue,
+        url: urlValue,
+    }
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    fetchBookmarks();
+    bookmarkForm.reset();
+    websiteNameEl.focus();
 }
 
 // Event Listener
 bookmarkForm.addEventListener('submit',storeBookmark);
+
+// On Load
+fetchBookmarks();
